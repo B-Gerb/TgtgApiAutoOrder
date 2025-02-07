7927489d796a88a7c9ffb6351381c1e6b3cc5c47
B-Gerb <gerber.br@northeastern.edu>
2025-02-07 17:12:06 -0500

    Created aws to more central, created creatingTgtgCommands to create the file, and now tgtgClass is the file/scrypt that will be executed on the aws machine

--- creatingTgtgCommands.py
+++ creatingTgtgCommands.py
@@ -1,7 +1,7 @@
 from tgtg import TgtgClient
 import sys
 import os
-from datetime import datetime
+from datetime import datetime,timezone
 import time
 import tgtgClass
 """
@@ -12,12 +12,17 @@ WILL TAKE USER INPUT TO EITHER
 2. GET NOTIFCIATIONS FROM A STORE
 
 """
-class tgtgTesting:
+class tgtgCommands:
 
-    def __init__(self):
+    def __init__(self, timezone=timezone.utc):
+        self.timezone = timezone
         self.client = None
 
+
+    #Token creation is done through users computer not AWS
     def startUp(self, emailToUse):
+        if os.path.exists("commands.txt"):
+            os.remove("commands.txt")
         if emailToUse == "skip":
             return self.createClient()
         client = TgtgClient(email=emailToUse)
@@ -37,39 +42,124 @@ class tgtgTesting:
         access_token = reading.readline().strip()
         refresh_token = reading.readline().strip()
         cookie = reading.readline().strip()
+        reading.close()
+        writing = open("commands.txt", "w")
+        writing.write(f"access_token: {access_token}\n")
+        writing.write(f"refresh_token: {refresh_token}\n")
+        writing.write(f"cookie: {cookie}\n")
+        writing.write("connection\n")
+        writing.close()
         client = TgtgClient(access_token=access_token, refresh_token=refresh_token, cookie=cookie)
         return client
 
 
+    def creatingNotfication(self):
+        writing = open("commands.txt", "a")
+        items = client.get_items()
+        possibleOrders = {}
+        currOrder = 1
+        for item in items:
+            item = client.get_item(item['item']['item_id'])
+            possibleOrders[currOrder] = item
+            print(f"Option {currOrder}: {item['store']['name_name']}")
+            currOrder += 1
+        print("_"*50)
+        print("Type *all* to see all possible options or q to quit")
+        while True:
+            userChoice = input("Which option would you like to be notified about?")
+            if userChoice.isdigit():
+                if int(userChoice) not in possibleOrders:
+                    print("Invalid option")
+                    continue
+                else:
+                    print(f"Store choosen is {possibleOrders[int(userChoice)]['store']['store_name']}")
+                    if input("To Confirm this store type y, to cancel type anything else") != 'y':
+                        continue
+                    print("Will notify you when the item is avaliable")
+                    while True:
+                        choice = input("Will check availability for how long in hours?")
+                        if not choice.isdigit() or int(choice) < 0:
+                            print("Invalid time")
+                            continue
+                        else:
+                            duration = int(choice)
+                            break
+                    writing.write("item_id:" + possibleOrders[int(userChoice)]['item']['item_id'] + "\n")
+                    writing.write("duration:" + str(duration) + "\n")
+                    writing.write("type:notify\n")
+
+
+
+
     def orderAnItem(self, item):
-        print(f"Attempting to order from store {item['store']['store_name']}")
-        print("Starting ordering process")
-        failing = True
-        while failing:
-            try:    
-                order = self.client.create_order(item['item']['item_id'], 1)
-                print(order)
-                print("order created")
-                failing = False
-                return order
-            except:
-                print("Failed to order")
-            time.sleep(3)
-        print(order)
+        writing = open("commands.txt", "a")
+        items = client.get_items()
+        possibleOrders = {}
+        currOrder = 1
+        for item in items:
+            item = client.get_item(item['item']['item_id'])
+            if 'pickup_interval' not in item or 'next_sales_window_purchase_start' not in item:
+                continue 
+            print(item['store']['store_name'])
+            startInt = datetime.strptime(item['pickup_interval']['start'], "%Y-%m-%dT%H:%M:%SZ").astimezone(self.timezone)
+            endInt = datetime.strptime(item['pickup_interval']['end'], "%Y-%m-%dT%H:%M:%SZ").astimezone(self.timezone)
+            salesWindow = datetime.strptime(item['next_sales_window_purchase_start'], "%Y-%m-%dT%H:%M:%SZ").astimezone(self.timezone)
+            print(f"Option {currOrder}: At store {item['store']['store_name']} \n" +
+                f"Start time: {startInt}, end time: {endInt} ")
+            print(f"Next Sales window is at: {salesWindow}")
+            possibleOrders[currOrder] = item
+            currOrder += 1
+        print("_"*50)
+        print("Type *all* to see all possible options or q to quit")
+        while True:
+            order = (input("Which option would you like to order?"))
+
+            if not order.isdigit() or (int(order) not in possibleOrders):
+                if order.isdigit():
+                    print("Invalid option")
+                    continue
+                elif order == "all":
+                    for item in possibleOrders:
+                        print(f"Option {item}: At store {possibleOrders[item]['store']['store_name']}")
+                elif order == 'q':
+                    break
+                else:
+                    print("Invalid option")
+                    continue
+            else:
+                print(f"Ordering from store {order['store']['store_name']}")
+                if input("To Confirm this store type y, to cancel type anything else") != 'y':
+                    continue
+                order = int(order)
+                order = possibleOrders[order]
+                print("How long would you like to attempt the order?")
+                print("Duration will be after the next sales window")
+                while (time := input("Duration in minutes: ")):
+                    if not time.isdigit():
+                        print("Invalid time")
+                        continue
+                    time = int(time)
+                    if time < 0:
+                        print("Invalid time")
+                        continue
+                    break
+            break
+        writing.wrote("item_id:" + order['item']['item_id'] + "\n")
+        writing.write("duration:" + str(time) + "\n")
+        writing.write("type:order\n")
+        writing.close()
+            
+                
 
 
-    def createNotification(self, item):
-        print(f"Attempting to order from store {item['store']['store_name']}")
-        print("Starting ordering process")
-        failing = True
-        while failing:
-            try:    
-                order = self.client.create_order(item['item']['item_id'], 1)
-                print(order)
-                print("order created")
-                failing = False
-                return order
-            except:
-                print("Failed to order")
-            time.sleep(3)
-        print(order)
+
+def creatingCommands():
+    #this will create commands from the user to be executed possible command string
+    """
+    skip 
+    order xyz 3 hours starting at 3:00 5 second intervals notfication system waitTime if confirmed
+
+    """
+    setUp = tgtgTesting()
+    client = setUp.startUp(input("Enter your email or type skip of tokens.txt already contains keys: ")) 
+    
--- tgtgClass.py
+++ tgtgClass.py
@@ -1,59 +1,114 @@
 from tgtg import TgtgClient
 import sys
 import os
-from datetime import datetime
+from datetime import datetime,timezone
 import time
 
+
+#Class for commands to go through tgtg
+#This file will go AWS along with tokens.txt
+#this is the script that will run, commands.txt will be the commands
 class tgtgTesting:
+    #intalize
 
     def __init__(self):
         self.client = None
 
-    def startUp(self, emailToUse):
-        if emailToUse == "skip":
-            return self.createClient()
-        client = TgtgClient(email=emailToUse)
-        credentials = client.get_credentials()
-        if os.path.exists("tokens.txt"):
-            os.remove("tokens.txt")
-        
-        writing = open("tokens.txt", "w")
-        writing.write(credentials['access_token'] + "\n")
-        writing.write(credentials['refresh_token'] + "\n")
-        writing.write(credentials['cookie'] + "\n")
-        writing.close()
-        return self.createClient()
-
-    def createClient(self):
-        reading = open("tokens.txt", "r")
-        access_token = reading.readline().strip()
-        refresh_token = reading.readline().strip()
-        cookie = reading.readline().strip()
+    #creates the client
+    def createClient(access_token, refresh_token, cookie):
         client = TgtgClient(access_token=access_token, refresh_token=refresh_token, cookie=cookie)
-        return client
+        notifyUser("connection", message= "Connection established")
 
 
-    def orderItem(self, item):
-        print(f"Attempting to order from store {item['store']['store_name']}")
-        print("Starting ordering process")
-        failing = True
-        while failing:
+    """
+    Returns the order object if successful, otherwise returns "Failed to order"
+    item: the item object
+    duration: the time in minutes you want to wait to order the item after the next sales window
+    """
+    def orderAnItem(self, item, duration):
+        time = item['next_sales_window_purchase_start']
+        target_date = datetime.fromisoformat("time").replace(tzinfo=timezone.utc)
+        now = datetime.now(timezone.utc)
+        time_to_wait = (target_date - now).total_seconds()
+        time_to_wait = startTime - datetime.now(timezone.utc)
+        time.sleep(time_to_wait.total_seconds()-3)
+        duration +=3
+        firstSpeed = 600
+        while firstSpeed > 0:
             try:    
                 order = self.client.create_order(item['item']['item_id'], 1)
-                print(order)
-                print("order created")
-                failing = False
-                return order
+                notifyUser("order", order, message= "successful order")
+                return
             except:
-                print("Failed to order")
-            time.sleep(3)
-        print(order)
-#If you want to start the client with a new email, use the startUp function
+                time.sleep(1)
+                firstSpeed -= 1
+        secondSpeed = (duration*60) - 600
+        while secondSpeed > 0:
+            try:    
+                order = self.client.create_order(item['item']['item_id'], 1)
+                notifyUser("order", order, message= "successful order")
+                return
+            except:
+                time.sleep(10)
+                secondSpeed -= 1
+        notifyUser("order", message= "Failed to order")
+         
 
+
+    """
+    item_id: the id of the item you want to be notified about
+    duration: the time in minutes you want to be notified for in minutes
+    """
+    def notifyWhenAvaliable(self, item_id, duration):
+        duration = duration*60
+
+        while duration > 0:
+            item = client.get_item(item_id)
+            if item['items_available'] > 0:
+                notifyUser("notify", item, "Item is avaliable")
+                return 
+            time.sleep(10)
+            duration -= 10
+        notifyUser("notify", message= "Never became avaliable")
+        
+             
+
+
+
+    """
+    type: the type of notification, either order or notify
+    """
+    def notifyUser(self, type, item=None, message = None):
+        if type == "order":
+            print('place holder for order')
+        elif type == "notify":
+            print('place holder for avaiable')
+        elif type == "connection":
+            print('place holder for connection')
+        elif type == "abort":
+            print('place holder for abort')
+        else:
+            return "Invalid type"
+
+    def abortOrder(self, order_id):
+        client.abort_order(order_id)
+        notifyUser("abort", message= "Order aborted")
+
+#If you want to start the client with a new email, use the startUp function
+"""
+Commandline starts with
+access_token: tokenhere
+refresh_token: tokenhere
+cookie: tokenhere
+connection
+
+"""
 def main():
     lines =  sys.stdin.readlines()
     for line in lines:
         print(line)
+
+
 if __name__ == "__main__":
     main()
 
